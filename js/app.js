071a5c538f1d79e583231c3256a0abbb55b6467b
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-08-07 15:41:57 +0200

    Homepage Visual Elements Complete
    I have completed how the homepage looks
    
    I need to add some functionality to it and add the a tag description.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 console.log(d3);
+
+//Homepage Button Hover:
+let navBtns = document.querySelectorAll(".nav-btn");
+let tooltipHome = document.querySelector(".tooltip-home");
+
+navBtns.forEach((navBtn) => {
+  navBtn.addEventListener("mouseover", () => {
+    tooltipHome.className = "tooltip-home-show";
+    for (i = 0; i < homeTooltips.length; i++) {
+      if (navBtn.innerHTML === homeTooltips[i].heading) {
+        tooltipHome.innerHTML = homeTooltips[i].tooltipText;
+      }
+    }
+  });
+});
+
+navBtns.forEach((navBtn) => {
+  navBtn.addEventListener("mouseleave", () => {
+    tooltipHome.className = "tooltip-home";
+    tooltipHome.innerHTML = "";
+  });
+});
---
+++ js/data.js
@@ -0,0 +1,27 @@
+const homeTooltips = [
+  {
+    id: 0,
+    heading: "Mars",
+    tooltipText: "Mars",
+  },
+  {
+    id: 1,
+    heading: "Visual Marvel",
+    tooltipText: "VM",
+  },
+  {
+    id: 2,
+    heading: "Kepler Field",
+    tooltipText: "KF",
+  },
+  {
+    id: 3,
+    heading: "Blog Section",
+    tooltipText: "BS",
+  },
+  {
+    id: 4,
+    heading: "Design Section",
+    tooltipText: "DS",
+  },
+];
