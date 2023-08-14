6bc5b38f7d2be4286a60e9dabd8bab8259c8a246
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-08-14 10:59:43 +0200

    Added the data for my tooltips

--- js/data.js
+++ js/data.js
@@ -2,53 +2,59 @@ const homeTooltips = [
   {
     id: 0,
     heading: "Mars",
-    tooltipText: "Mars",
+    tooltipText: "Explore the weather on Mars and the different landing sites.",
   },
   {
     id: 1,
     heading: "Visual Marvel",
-    tooltipText: "VM",
+    tooltipText: "View The Picture Of The Day!",
   },
   {
     id: 2,
     heading: "Kepler Field",
-    tooltipText: "KF",
+    tooltipText:
+      "Explore the different planets within the Kepler Field and the data that NASA has stored on them.",
   },
   {
     id: 3,
     heading: "Blog Section",
-    tooltipText: "BS",
+    tooltipText:
+      "This section comprises of the blog posts that I have completed throughout Semester 2 (2023).",
   },
   {
     id: 4,
     heading: "Design Section",
-    tooltipText: "DS",
+    tooltipText:
+      "This section comprises of my design choices and intentions when creating this website.",
   },
 ];
 
 const tabDetails = [
   {
     title: "Homepage",
-    classOne: "",
+    classOne: "Return to the Homepage",
   },
   {
     title: "Mars",
-    classOne: "",
+    classOne: "Explore the weather on Mars and the different landing sites.",
   },
   {
     title: "Visual Marvel",
-    classOne: "",
+    classOne: "View The Picture Of The Day!",
   },
   {
     title: "Kepler Field",
-    classOne: "",
+    classOne:
+      "Explore the different planets within the Kepler Field and the data that NASA has stored on them.",
   },
   {
     title: "Blog Section",
-    classOne: "",
+    classOne:
+      "This section comprises of the blog posts that I have completed throughout Semester 2 (2023).",
   },
   {
     title: "Design Section",
-    classOne: "",
+    classOne:
+      "This section comprises of my design choices and intentions when creating this website.",
   },
 ];
