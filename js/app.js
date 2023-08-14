5a009f036d2d8b2edfe383d24593688d7c9c1058
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-08-14 13:18:50 +0200

    Page Updates
    Each page is updated accordingly to make sure the tooltips work
    
    Also, my design notes for my data visualisation elements were added

--- js/app.js
+++ js/app.js
@@ -68,3 +68,25 @@ if (heading.innerHTML === "Blog Section") {
     window.location.href = "#blog-two";
   });
 }
+let navBtnPages = document.querySelectorAll(".nav-btn-pages");
+let navBtnActive = document.querySelector(".nav-btn-pages active");
+let tooltipPages = document.querySelector(".tooltip-pages-show");
+
+navBtnPages.forEach((navPage) => {
+  navPage.addEventListener("mouseover", () => {
+    tooltipPages.className = "tooltip-pages";
+    for (i = 0; i < tabDetails.length; i++) {
+      if (navPage.innerHTML === tabDetails[i].title) {
+        tooltipPages.innerHTML = tabDetails[i].info;
+      }
+    }
+  });
+});
+
+navBtnPages.forEach((navPage) => {
+  navPage.addEventListener("mouseleave", () => {
+    for (i = 0; i < tabDetails.length; i++) {
+      tooltipPages.className = "tooltip-pages-show";
+    }
+  });
+});
--- js/data.js
+++ js/data.js
@@ -31,30 +31,33 @@ const homeTooltips = [
 
 const tabDetails = [
   {
+    id: 0,
     title: "Homepage",
-    classOne: "Return to the Homepage",
+    info: "Return to the Homepage",
   },
   {
+    id: 1,
     title: "Mars",
-    classOne: "Explore the weather on Mars and the different landing sites.",
+    info: "Explore the weather on Mars and the different landing sites.",
   },
   {
+    id: 2,
     title: "Visual Marvel",
-    classOne: "View The Picture Of The Day!",
+    info: "View The Picture Of The Day!",
   },
   {
+    id: 3,
     title: "Kepler Field",
-    classOne:
-      "Explore the different planets within the Kepler Field and the data that NASA has stored on them.",
+    info: "Explore the different planets within the Kepler Field and the data that NASA has stored on them.",
   },
   {
+    id: 4,
     title: "Blog Section",
-    classOne:
-      "This section comprises of the blog posts that I have completed throughout Semester 2 (2023).",
+    info: "This section comprises of the blog posts that I have completed throughout Semester 2 (2023).",
   },
   {
+    id: 5,
     title: "Design Section",
-    classOne:
-      "This section comprises of my design choices and intentions when creating this website.",
+    info: "This section comprises of my design choices and intentions when creating this website.",
   },
 ];
