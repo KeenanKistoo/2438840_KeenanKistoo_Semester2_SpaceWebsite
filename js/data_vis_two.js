784aaad2d9ba2c58e1c9ea285eb34c2bf92bbfdd
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-09-11 00:45:43 +0200

    Created the table and entered the data
    I used a very vague and honestly unhelpful template to create a table for my data.
    
    After a few hours of playing around with d3 and its documentation i got it though!
    
    need to apply styles to it now

---
+++ js/data_vis_two.js
@@ -0,0 +1,43 @@
+const techUrl =
+  "https://api.nasa.gov/DONKI/FLR?startDate=2023-07-01&endDate=2023-08-01&api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH";
+
+d3.json(techUrl).then((data) => {
+  console.log(data);
+
+  const table = d3
+    .select("#table-container")
+    .append("table")
+    .attr("class", "data-table");
+
+  const thead = table.append("thead").attr("class", "headings");
+
+  thead
+    .append("tr")
+    .selectAll("th")
+    .data([
+      "Active Region No.",
+      "Begin Time",
+      "Peak Time",
+      "End Time",
+      "Class Type",
+    ])
+    .enter()
+    .append("th")
+    .text((d) => d);
+
+  const tbody = table.append("tbody");
+  const rows = tbody.selectAll("tr").data(data).enter().append("tr");
+
+  const cells = rows
+    .selectAll("td")
+    .data((d) => [
+      d.activeRegionNum,
+      d.beginTime,
+      d.peakTime,
+      d.endTime,
+      d.classType,
+    ])
+    .enter()
+    .append("td")
+    .text((d) => d);
+});
