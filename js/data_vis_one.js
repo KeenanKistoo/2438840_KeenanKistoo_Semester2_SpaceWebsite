66bf6c45557edab8d8f4a58ef22be4cf7a087342
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-10-09 01:35:20 +0200

    Added interactivity
    The user has a choice of three days to look at
    
    there is now a tooltip

--- js/data_vis_one.js
+++ js/data_vis_one.js
@@ -1,11 +1,11 @@
 let metUrl =
-  "https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-09-07&end_date=2023-09-08&api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH";
+  "https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-10-03&end_date=2023-10-06&api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH";
 let apiKey = "imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH";
 
 //Create Dimensions
 let HEIGHT = 700;
 let WIDTH = 1000;
-let MARGIN = { top: 50, right: 500, bottom: 50, left: 500 };
+let MARGIN = { top: 50, right: 500, bottom: 60, left: 500 };
 
 //Create SVG
 let svg = d3
@@ -24,14 +24,21 @@ const year = today.getFullYear();
 
 const formattedDate = `${year}-0${month}-0${day}`;
 
-console.log(`Today's date is ${formattedDate}`);
+//console.log(`Today's date is ${formattedDate}`);
+
+let date = ["2023-10-03", "2023-10-04", "2023-10-05"];
+let index = 0;
+
+let octThree = document.querySelector("#oct-three");
+let octFour = document.querySelector("#oct-four");
+let octFive = document.querySelector("#oct-five");
 
 //Determine max and min scales and creating the axes:
 
 d3.json(metUrl).then((data) => {
-  let neo = data.near_earth_objects["2023-09-08"];
-  console.log(neo);
-  console.log(neo[0]);
+  let neo = data.near_earth_objects[date[index]];
+  //console.log(neo);
+  //console.log(neo[0]);
 
   let xMax = neo[0].close_approach_data[0].miss_distance.kilometers;
   let yMax = neo[0].absolute_magnitude_h;
@@ -93,7 +100,41 @@ d3.json(metUrl).then((data) => {
   }
   createXAxis();
   createYAxis();
+  //Creating tooltip
+  let tooltip = d3
+    .select("#tooltip")
+    .append("section")
+    .style("opacity", 1)
+    .style("width", "170px")
+    .style("height", "100px")
+    .style("border-radius", "5px")
+    .style("padding", "12px")
+    .style("background-color", "#000")
+    .style("color", "#fff")
+    .style("position", "relative");
 
+  //function to show and hide tooltip
+  function showTooltip(d) {
+    tooltip
+      .transition()
+      .duration(500)
+      .style("opacity", 1)
+      .style("left", d3.pointer(event)[0] + 340 + "px")
+      .style("top", d3.pointer(event)[1] - 750 + "px");
+
+    //tooltip.html("Country: " + d.country);
+  }
+
+  function hideTooltip() {
+    tooltip.style("opacity", 0);
+  }
+
+  function moveTooltip() {
+    tooltip
+      .style("left", d3.pointer(event)[0] + 340 + "px")
+      .style("top", d3.pointer(event)[1] - 750 + "px");
+  }
+  //Creating bubbles
   function CreateBubbles(data) {
     svg
       .append("g")
@@ -114,11 +155,34 @@ d3.json(metUrl).then((data) => {
       .style("fill", (d) => colourScale(d.is_potentially_hazardous_asteroid))
       .style("stroke", "#000")
       .style("stroke-width", "1px")
-      .style("opacity", 0.7);
-    console.log(data);
+      .style("opacity", 0.7)
+      .on("mouseover", (event, datum) => showTooltip(datum))
+      .on("mousemove", (event, datum) => moveTooltip())
+      .on("mouseout", (event, datum) => hideTooltip());
+
+    //console.log(data);
   }
 
-  const myData = data.near_earth_objects["2023-09-07"];
+  const myData = data.near_earth_objects[date[index]];
   CreateBubbles(myData);
+  octThree.addEventListener("click", () => {
+    svg.selectAll("circle").remove();
+    index = 0;
+    console.log("clicked");
+    const myData = data.near_earth_objects[date[index]];
+    CreateBubbles(myData);
+  });
+  octFour.addEventListener("click", () => {
+    svg.selectAll("circle").remove();
+    index = 1;
+    const myData = data.near_earth_objects[date[index]];
+    CreateBubbles(myData);
+  });
+  octFive.addEventListener("click", () => {
+    svg.selectAll("circle").remove();
+    index = 2;
+    const myData = data.near_earth_objects[date[index]];
+    CreateBubbles(myData);
+  });
 });
 //create tooltip
