dd9f2c6c9e2d2a532134692d4ca8da0f880a08c7
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-10-29 14:22:51 +0200

    Basic UI For Example Level

--- js/game_example.js
+++ js/game_example.js
@@ -79,3 +79,169 @@ function Timer() {
 }
 
 /* -----------------------------------------------------UI FOR THE LEFT HAND SIDE------------------------------------------------------- */
+/*---------------------Backgrounds----------------------*/
+example
+  .append("rect")
+  .attr("x", 60)
+  .attr("y", 155)
+  .attr("width", 400)
+  .attr("height", 230)
+  .attr("rx", 0)
+  .attr("ry", 0)
+  .style("fill", "black")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 0.1);
+example
+  .append("rect")
+  .attr("x", 500)
+  .attr("y", 155)
+  .attr("width", 400)
+  .attr("height", 230)
+  .attr("rx", 0)
+  .attr("ry", 0)
+  .style("fill", "black")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 0.1);
+example
+  .append("rect")
+  .attr("x", 60)
+  .attr("y", 400)
+  .attr("width", 400)
+  .attr("height", 230)
+  .attr("rx", 0)
+  .attr("ry", 0)
+  .style("fill", "black")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 0.1);
+example
+  .append("rect")
+  .attr("x", 500)
+  .attr("y", 400)
+  .attr("width", 400)
+  .attr("height", 230)
+  .attr("rx", 0)
+  .attr("ry", 0)
+  .style("fill", "black")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 0.1);
+/*-----------------Buttons and Information-------------------------- */
+let astroBtn = example
+  .append("circle")
+  .attr("cx", 250)
+  .attr("cy", 200)
+  .attr("r", 30)
+  .style("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 10);
+let astroHeading = example
+  .append("text")
+  .attr("id", "timer-txt")
+  .attr("x", 140)
+  .attr("y", 270)
+  .text("Astronaut Timeline:")
+  .classed("game-head", true)
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white");
+
+let flightBtn = example
+  .append("circle")
+  .attr("cx", 700)
+  .attr("cy", 200)
+  .attr("r", 30)
+  .style("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 10);
+let flightHeading = example
+  .append("text")
+  .attr("id", "timer-txt")
+  .attr("x", 590)
+  .attr("y", 270)
+  .text("Air Traffic Timeline:")
+  .classed("game-head", true)
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white");
+
+let teleBtn = example
+  .append("circle")
+  .attr("cx", 250)
+  .attr("cy", 450)
+  .attr("r", 30)
+  .style("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 10);
+let teleHeading = example
+  .append("text")
+  .attr("id", "timer-txt")
+  .attr("x", 100)
+  .attr("y", 520)
+  .text("Telecommunications Timeline:")
+  .classed("game-head", true)
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white");
+
+let noneBtn = example
+  .append("circle")
+  .attr("cx", 700)
+  .attr("cy", 450)
+  .attr("r", 30)
+  .style("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 10);
+let noneHeading = example
+  .append("text")
+  .attr("id", "timer-txt")
+  .attr("x", 630)
+  .attr("y", 550)
+  .text("Notify None")
+  .classed("game-head", true)
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white");
+/* -----------------------------------------------------UI FOR THE RIGHT HAND SIDE------------------------------------------------------- */
+/*---------------------Backgrounds----------------------*/
+example
+  .append("rect")
+  .attr("x", 1000)
+  .attr("y", 155)
+  .attr("width", 400)
+  .attr("height", 480)
+  .attr("rx", 0)
+  .attr("ry", 0)
+  .style("fill", "black")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 0.1);
+/*-----------------Buttons and Information-------------------------- */
+let northBtn = example
+  .append("circle")
+  .attr("cx", 1210)
+  .attr("cy", 200)
+  .attr("r", 30)
+  .style("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 10);
+let northHeading = example
+  .append("text")
+  .attr("id", "timer-txt")
+  .attr("x", 1090)
+  .attr("y", 270)
+  .text("Northern Hemisphere")
+  .classed("game-head", true)
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white");
+
+let southBtn = example
+  .append("circle")
+  .attr("cx", 1210)
+  .attr("cy", 450)
+  .attr("r", 30)
+  .style("fill", "white")
+  .attr("stroke", "grey")
+  .attr("stroke-width", 10);
+let southHeading = example
+  .append("text")
+  .attr("id", "timer-txt")
+  .attr("x", 1090)
+  .attr("y", 550)
+  .text("Southern Hemisphere")
+  .classed("game-head", true)
+  .attr("font-size", "1.5rem")
+  .attr("fill", "white");
