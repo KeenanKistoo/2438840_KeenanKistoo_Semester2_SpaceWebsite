7bae18acd15464233f55dc61bbdf057abc838470
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-11-13 16:31:43 +0200

    Shapes and sizes is updated

--- js/mosaic.js
+++ js/mosaic.js
@@ -15,37 +15,64 @@ let svg = d3
   .append("g")
   .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`);
 
-d3.json(apodUrl).then((data) => {
-  console.log(data[0].url);
-  const tileSize = 140; // Adjust this value based on your desired size
+// d3.json(apodUrl).then((data) => {
+//   console.log(data[0].url);
+//   const tileSize = 140; // Adjust this value based on your desired size
 
-  // Calculate the number of columns dynamically based on the available width
-  const numColumns = Math.floor(WIDTH / tileSize);
+//   // Calculate the number of columns dynamically based on the available width
+//   const numColumns = Math.floor(WIDTH / tileSize);
+
+//   data.forEach((d, i) => {
+//     // Skip elements with YouTube videos
+//     if (d.media_type === "video") {
+//       return; // Skip this iteration of the loop
+//     }
+
+//     // Calculate adjusted index
+//     let adjustedIndex = i;
+//     if (i >= 24) {
+//       adjustedIndex -= 2; // Skip elements at indices 24 and 37
+//     }
+
+//     const col = adjustedIndex % numColumns;
+//     const row = Math.floor(adjustedIndex / numColumns);
+
+//     // Assuming 'url' is a property in each data element
+//     svg
+//       .append("image")
+//       .classed("mosaic-tile", true)
+//       .attr("id", `mos-${adjustedIndex}`)
+//       .attr("x", col * tileSize)
+//       .attr("y", row * tileSize)
+//       .attr("width", tileSize)
+//       .attr("height", tileSize)
+//       .attr("xlink:href", d.url);
+//   });
+// });
+
+d3.json(apodUrl).then((data) => {
+  const tileSize = Math.floor(WIDTH / 7); // Adjust this value based on your desired size
 
   data.forEach((d, i) => {
-    // Skip elements with YouTube videos
     if (d.media_type === "video") {
-      return; // Skip this iteration of the loop
+      return;
     }
 
-    // Calculate adjusted index
-    let adjustedIndex = i;
-    if (i >= 24) {
-      adjustedIndex -= 2; // Skip elements at indices 24 and 37
-    }
+    const col = i % 7;
+    const row = Math.floor(i / 7);
 
-    const col = adjustedIndex % numColumns;
-    const row = Math.floor(adjustedIndex / numColumns);
+    const x = col * tileSize;
+    const y = row * tileSize;
 
-    // Assuming 'url' is a property in each data element
     svg
       .append("image")
       .classed("mosaic-tile", true)
-      .attr("id", `mos-${adjustedIndex}`)
-      .attr("x", col * tileSize)
-      .attr("y", row * tileSize)
+      .attr("id", `mos-${i}`)
+      .attr("x", x)
+      .attr("y", y)
       .attr("width", tileSize)
       .attr("height", tileSize)
-      .attr("xlink:href", d.url);
+      .attr("xlink:href", d.url)
+      .attr("preserveAspectRatio", "xMidYMid slice");
   });
 });
