2e7779c1ac001cb5565be64742546f1e0f1c48c2
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-11-13 15:26:56 +0200

    All pictures are being displayed
    Pictures are there, i just need to make the spaces smaller in between

--- js/mosaic.js
+++ js/mosaic.js
@@ -1,8 +1,8 @@
 apodUrl =
-  "https://api.nasa.gov/planetary/apod?api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH&start_date=2023-10-01&end_date=2023-11-11";
+  "https://api.nasa.gov/planetary/apod?api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH&start_date=2023-09-30&end_date=2023-11-12";
 
 //Create Dimensions
-let HEIGHT = 700;
+let HEIGHT = 1000;
 let WIDTH = 1000;
 let MARGIN = { top: 50, right: 500, bottom: 60, left: 500 };
 
@@ -16,17 +16,36 @@ let svg = d3
   .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`);
 
 d3.json(apodUrl).then((data) => {
-  console.log(data[24].url);
+  console.log(data[0].url);
+  const tileSize = 140; // Adjust this value based on your desired size
+
+  // Calculate the number of columns dynamically based on the available width
+  const numColumns = Math.floor(WIDTH / tileSize);
+
   data.forEach((d, i) => {
+    // Skip elements with YouTube videos
+    if (d.media_type === "video") {
+      return; // Skip this iteration of the loop
+    }
+
+    // Calculate adjusted index
+    let adjustedIndex = i;
+    if (i >= 24) {
+      adjustedIndex -= 2; // Skip elements at indices 24 and 37
+    }
+
+    const col = adjustedIndex % numColumns;
+    const row = Math.floor(adjustedIndex / numColumns);
+
     // Assuming 'url' is a property in each data element
     svg
       .append("image")
       .classed("mosaic-tile", true)
-      .attr("id", `mos-${i}`)
-      .attr("x", (i % 7) * 100) // Adjust these values as needed for positioning
-      .attr("y", Math.floor(i / 7) * 100) // Adjust these values as needed for positioning
-      .attr("width", 100) // Adjust these values as needed for image size
-      .attr("height", 100) // Adjust these values as needed for image size
+      .attr("id", `mos-${adjustedIndex}`)
+      .attr("x", col * tileSize)
+      .attr("y", row * tileSize)
+      .attr("width", tileSize)
+      .attr("height", tileSize)
       .attr("xlink:href", d.url);
   });
 });
