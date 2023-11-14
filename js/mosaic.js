2ff4c026edd072f8ad2d73cd1f46b28dfde7dafe
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-11-14 11:21:57 +0200

    Added different headings needed

--- js/mosaic.js
+++ js/mosaic.js
@@ -1,65 +1,33 @@
 apodUrl =
-  "https://api.nasa.gov/planetary/apod?api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH&start_date=2023-09-30&end_date=2023-11-12";
+  "https://api.nasa.gov/planetary/apod?api_key=imb3uQNJIXqz8tyBuMd7uSf4lgkqVrULlw2s6GCH&start_date=2023-09-27&end_date=2023-11-12";
 
 //Create Dimensions
 let HEIGHT = 1000;
 let WIDTH = 1000;
-let MARGIN = { top: 50, right: 500, bottom: 60, left: 500 };
+let MARGIN = { top: 0, right: 500, bottom: -290, left: 380 };
 
-//Create SVG
 let svg = d3
   .select("#mosaic")
   .append("svg")
   .attr("height", HEIGHT + MARGIN.bottom + MARGIN.top)
   .attr("width", WIDTH + MARGIN.left + MARGIN.right)
   .append("g")
-  .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`);
-
-// d3.json(apodUrl).then((data) => {
-//   console.log(data[0].url);
-//   const tileSize = 140; // Adjust this value based on your desired size
-
-//   // Calculate the number of columns dynamically based on the available width
-//   const numColumns = Math.floor(WIDTH / tileSize);
-
-//   data.forEach((d, i) => {
-//     // Skip elements with YouTube videos
-//     if (d.media_type === "video") {
-//       return; // Skip this iteration of the loop
-//     }
-
-//     // Calculate adjusted index
-//     let adjustedIndex = i;
-//     if (i >= 24) {
-//       adjustedIndex -= 2; // Skip elements at indices 24 and 37
-//     }
-
-//     const col = adjustedIndex % numColumns;
-//     const row = Math.floor(adjustedIndex / numColumns);
-
-//     // Assuming 'url' is a property in each data element
-//     svg
-//       .append("image")
-//       .classed("mosaic-tile", true)
-//       .attr("id", `mos-${adjustedIndex}`)
-//       .attr("x", col * tileSize)
-//       .attr("y", row * tileSize)
-//       .attr("width", tileSize)
-//       .attr("height", tileSize)
-//       .attr("xlink:href", d.url);
-//   });
-// });
+  .attr("transform", `translate(${MARGIN.left}, ${MARGIN.top})`)
+  .style("padding-bottom", "-20rem");
 
 d3.json(apodUrl).then((data) => {
-  const tileSize = Math.floor(WIDTH / 7); // Adjust this value based on your desired size
+  const numColumns = 9;
+  const numRows = 5;
+
+  const tileSize = Math.floor(WIDTH / numColumns + 18);
 
   data.forEach((d, i) => {
     if (d.media_type === "video") {
       return;
     }
 
-    const col = i % 7;
-    const row = Math.floor(i / 7);
+    const col = i % numColumns;
+    const row = Math.floor(i / numColumns);
 
     const x = col * tileSize;
     const y = row * tileSize;
@@ -75,4 +43,7 @@ d3.json(apodUrl).then((data) => {
       .attr("xlink:href", d.url)
       .attr("preserveAspectRatio", "xMidYMid slice");
   });
+
+  d3.select("#mos-45").attr("x", 645).attr("y", 516);
+  d3.select("#mos-46").attr("y", 387);
 });
